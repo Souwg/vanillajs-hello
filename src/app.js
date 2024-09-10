4933c1aa08d6c23f49b4df0549d4741e66e8d5ce
Sousan Wong <sansanvzla@gmail.com>
2024-09-10 11:48:05 -0400

    pokercard

--- src/app.js
+++ src/app.js
@@ -5,15 +5,23 @@ function randomletra(){
   const indiceAleatorio = Math.floor(Math.random() * letras.length);
   return letras[indiceAleatorio];
 }
+
 function randomIcono(){
+  const iconos = ['♦', '♥', '♠', '♣']
   const iconoAleatorio = Math.floor(Math.random() * iconos.length);
-  return iconos[iconoAleatorio];
+  const icono = iconos[iconoAleatorio];
+  if (icono === '♥' || icono === '♦') {
+    return `<span class="red">${icono}</span>`;
+  } else {
+    return icono;
+  }
 }
+
 window.onload = function () {
   let letterNumber1 = document.querySelector("#letterNUMBER")
   letterNumber1.innerHTML = randomletra()
   let iconorow1 = document.querySelector("#iconos2")
-  let prueba = iconorow1.innerHTML = randomIcono()
+  let igualarAmbosIconos = iconorow1.innerHTML = randomIcono()
   let iconorow2 = document.querySelector("#iconos1")
-  iconorow2.innerHTML = prueba
+  iconorow2.innerHTML = igualarAmbosIconos
 }
